9df36887b7d376e87288dc52086baabfafa0beaa
Gouldian0120 <86667277+Gouldian0120@users.noreply.github.com>
2017-04-19 20:41:58 -0400

    added text to home page

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './Header';
-import Footer from './Footer';
 
 const Home = ()=> {
   return (
-    <Header />
-    <Footer />
-  );
+    <div>
+        oompa loompa
+        <Header />
+    </div>
+    );
 };
 
 export default Home;
