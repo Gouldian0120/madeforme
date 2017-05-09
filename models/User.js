6c8ebd8023e101664b376c789ec5b33b3a8e8d85
Gouldian0120 <86667277+Gouldian0120@users.noreply.github.com>
2017-05-09 16:44:23 -0400

    stripe amount fixed

--- models/User.js
+++ models/User.js
@@ -4,35 +4,17 @@ const User = conn.define('user', {
     firstName: {
         type: conn.Sequelize.STRING,
         validate: {
-            requiresContent: function (value) {
-                if (value === "" || value === null) {
-                    throw new Error('Validation error');
+            requiresContent: function(value) {
+                if(value === "" || value === null) {
+                    throw new Error('Validation error')
                 }
             }
         }
     },
 
-    lastName: {
-        type: conn.Sequelize.STRING,
-        validate: {
-            notEmpty: { msg: 'Please complete Address' }
-        }
-    },
-    email: {
-        type: conn.Sequelize.STRING,
-        validate: {
-            isEmail: true,
-            notEmpty: { msg: 'Please complete Email' }
-        }
-    },
-    password: {
-        type: conn.Sequelize.STRING,
-        validate: {
-            notEmpty: { msg: 'Please complete Password' }
-        }
-    },
-}, {
-        indexes: [{ unique: true, fields: ['email'] }]
-    });
+    lastName: conn.Sequelize.STRING,
+    email: conn.Sequelize.STRING,
+    password: conn.Sequelize.STRING,
+});
 
 module.exports = User;
